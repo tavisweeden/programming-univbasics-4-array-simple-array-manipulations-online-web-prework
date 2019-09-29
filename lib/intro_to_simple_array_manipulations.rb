3d2ad6c90b1cc2272a0baab5c28e1a40f5070525
Tavis Weeden <tavisweeden@gmail.com>
2019-09-29 16:23:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,13 +53,11 @@ def using_insert(list_of_programming_languages, another_language)
 end  
 
 def using_uniq(haircuts)
-  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
   new_array = haircuts.uniq
 
 end
 
 def using_flatten(instruments)
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
   flat_array = instruments.flatten
 
 end
