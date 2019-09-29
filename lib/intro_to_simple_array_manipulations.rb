f59812bd3d0c044a2bbc428e760707419e1ad523
Tavis Weeden <tavisweeden@gmail.com>
2019-09-29 17:35:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -63,7 +63,7 @@ def using_delete(instructors, no_offense_steven)
 end  
 
 define using_delete_at(famous_robot, deleted_robot)
-  deleted_robot = famous_robot.delete_at(2)
+  deleted_robot = famous_robot.delete_at[2]
   
 end
 
